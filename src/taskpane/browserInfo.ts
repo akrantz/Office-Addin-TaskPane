fdee94cf33085a8380afb96115c5ae35cf53d9f3
Adam Krantz <Adam.Krantz@microsoft.com>
2020-07-28 07:44:14 -0700

    prettier

--- src/taskpane/browserInfo.ts
+++ src/taskpane/browserInfo.ts
@@ -2,34 +2,30 @@ import * as bowser from "bowser";
 
 /* global navigator */
 
-export class BrowserInfo
-{
-    private name: string;
-    private version: string;
+export class BrowserInfo {
+  private name: string;
+  private version: string;
 
-    get Name(): string
-    { 
-        return this.name;
+  get Name(): string {
+    return this.name;
+  }
+
+  get Version(): string {
+    return this.version;
+  }
+
+  public constructor(userAgent?: string) {
+    if (!userAgent) {
+      if (navigator != null) {
+        userAgent = navigator.userAgent;
+      }
     }
-    
-    get Version(): string
-    {
-        return this.version;
+
+    if (userAgent) {
+      const result = bowser.getParser(userAgent).getResult();
+
+      this.name = result.browser.name;
+      this.version = result.browser.version;
     }
-
-    public constructor(userAgent?: string)
-    {
-        if (!userAgent) {
-            if (navigator != null) {
-                userAgent = navigator.userAgent
-            }
-        }
-
-        if (userAgent) {
-            const result = bowser.getParser(userAgent).getResult();
-
-            this.name = result.browser.name;
-            this.version = result.browser.version;
-        }
-    }
+  }
 }
